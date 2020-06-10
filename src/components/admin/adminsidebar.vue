70510160d4939e541ead8e11af7cbcc3fff94a02
Henry Eze <henryeze019@gmail.com>
2020-06-10 13:10:09 -0700

    upload videos fully functional

---
+++ src/components/admin/adminsidebar.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav class="admin_sidebar h_100 p-1">
+    <div class="text-left pt-3 pl-2 border_bottom">
+        <bluestreak />
+     </div>
+     <ul class="pt-5 pl-0 or-0">
+     <li :class="$router.history.current.fullPath === path.path? inactiveClass + 'active border_radius' : inactiveClass" v-for="(path, index) in paths" :key="index">
+     <i :class="'mdi  mr-3 ' + path.icon">
+     </i>
+     <span>
+     {{path.name}}
+     </span>
+     </li>
+     </ul>
+    </nav>
+</template>
+<script>
+
+export default {
+    data: () => {
+        return {
+            paths: [
+                {
+                    name: 'Dashboard',
+                    path: '/admin-dashboard',
+                    icon: 'mdi-view-dashboard'
+                },
+                {
+                    name: 'Uploads',
+                    path: '/video-upload',
+                    icon: 'mdi-upload'
+                },
+                {
+                    name: 'Movies',
+                    path: '/admin-view-movies',
+                    icon: 'mdi-movie-open'
+                }
+            ],
+            inactiveClass: 'text-left pl-3 p-2 d-flex align-items-center mt-2 '
+        }
+    },
+    beforeMount() {
+        console.log()
+    },
+}
+</script>
--- src/layouts/adminlayout.vue
+++ src/layouts/adminlayout.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
+    <div class="d-flex h_100" >
+    <sidebar class="col-2"/>
+    <div class="col-10 admin_body h_100 p-0 overflow"> 
+    <navbar />
+    <div class="route_body overflow">
         <router-view></router-view>
+     </div>
+    </div>
     </div>
 </template>
+<script>
+import sidebar from '../components/admin/adminsidebar.vue'
+import navbar from '../components/admin/adminnavbar.vue'
+export default {
+    components: {
+        sidebar,
+        navbar
+    }
+    
+}
+</script>
+<style lang="scss">
+ @import "../style/admin.scss";
+ @import "../style/adminresponsive.scss";
+</style>
